4dc95b250f9b4849ebbb8681e38a7a97177a9e24
John Paul Wile <botsone@gmail.com>
2020-01-13 10:59:05 -0700

    removed console logging of data

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const Snooper = require('reddit-snooper')
-const Chokidar = require('chokidar')
-const Request = require('request')
-const Fs = require('fs')
-const Imgur = require('imgur')
+const snooper = require('reddit-snooper')
+const chokidar = require('chokidar')
+const request = require('request')
+const fs = require('fs')
+const imgur = require('imgur')
+const axios = require('axios')
 
 require('dotenv').config()
 
 // Set global vars
-let albumId = 'oiYHRHc'
 let author
-let title
+let imageTitle
 let permalink
+let imageId
+let postUrl
+let albumId = process.env.IMGUR_ALBUM_ID
 
-Imgur.setCredentials(
+imgur.setCredentials(
     process.env.IMGUR_USERNAME, 
     process.env.IMGUR_PASSWORD, 
     process.env.IMGUR_CLIENT_ID
 )
 
-snooper = new Snooper({
+Snooper = new snooper({
     username: process.env.REDDIT_USERNAME,
     password: process.env.REDDIT_PASSWORD,
     app_id: process.env.REDDIT_CLIENT_ID,
@@ -29,7 +32,7 @@ snooper = new Snooper({
     api_requests_per_minuite: 60 // api requests will be spread out in order to play nicely with Reddit
 })
 
-snooper.watcher.getPostWatcher('botsone') // blank argument or 'all' looks at the entire website
+Snooper.watcher.getPostWatcher('botsone') // blank argument or 'all' looks at the entire website
     .on('post', function(post) {
         // comment is a object containing all comment data
         console.log(post)
@@ -41,18 +44,18 @@ snooper.watcher.getPostWatcher('botsone') // blank argument or 'all' looks at th
 
 			// Grab the author, title, and URL
 			author = post.data.author
-			title = post.data.title
+			imageTitle = post.data.title
 			permalink = post.data.permalink
 			
 			// Download the new post
 			console.log('New post detected')
-			Request(post.data.url).pipe(Fs.createWriteStream("./pics/newestPost.jpg"))
+			request(post.data.url).pipe(fs.createWriteStream("./pics/newestPost.jpg"))
 		}
     })
     .on('error', console.error)
 
 // event listener to watch for new files in pics dir
-Chokidar.watch('./pics/newestPost.jpg', {
+chokidar.watch('./pics/newestPost.jpg', {
     ignored: /(^|[\/\\])\../,
     usepolling: true,
     interval: 10000,
@@ -63,12 +66,28 @@ Chokidar.watch('./pics/newestPost.jpg', {
     console.log(event);
 
     // read the media
-    var b64content = Fs.readFileSync('./pics/newestPost.jpg', { encoding: 'base64' })
+    var b64content = fs.readFileSync('./pics/newestPost.jpg', { encoding: 'base64' })
     let description = `New post from /r/AmoledBackgrounds from user ${author}. Upvote the post here: www.reddit.com${permalink} #wallpapers #backgrounds #amoled`
 
-    Imgur.uploadBase64(b64content, albumId, title, description)
+    imgur.uploadBase64(b64content, null, imageTitle, description)
     .then(function (json) {
-        console.log(json.data);
+        imageId = json.data.id
+
+        axios({
+            method: 'post',
+            url: `https://api.imgur.com/3/gallery/image/${imageId}`,
+            headers: {
+                'Authorization': process.env.IMGUR_ACCESS_TOKEN
+            },
+            data: {
+              title: imageTitle,
+              tags: 'amoled, wallpapers, backgrounds'
+            }
+          }).then((response) => {
+            console.log(response);
+          }, (error) => {
+            console.log(error);
+          });
     })
     .catch(function (err) {
         console.error(err.message);
